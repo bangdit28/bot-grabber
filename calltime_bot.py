bfe3b02a58937075859858dbfa3823add212429b
bangdit28 <adityahikmahtiar866@gmail.com>
2026-02-08 22:42:02 +0700

    Add initial CallTime bot implementation
    Initial implementation of the CallTime bot with a basic structure for SMS grabbing.

---
+++ calltime_bot.py
@@ -0,0 +1,9 @@
+import requests, time, os
+from bs4 import BeautifulSoup
+
+def run_calltime():
+    print("LOG: CallTime Service Started...")
+    # ... isi logika grabber SMS calltime lo ...
+    while True:
+        # Loop buat nge-grab SMS
+        time.sleep(2)
