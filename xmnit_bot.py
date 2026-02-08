da82869b4210c20bd180eabbda9d412476167736
bangdit28 <adityahikmahtiar866@gmail.com>
2026-02-08 22:42:25 +0700

    Initialize xmnit_bot.py with basic structure

---
+++ xmnit_bot.py
@@ -0,0 +1,8 @@
+import requests, time, os
+
+def run_xmnit():
+    print("LOG: X-MNIT Service Started...")
+    while True:
+        # Loop buat mantau Firebase perintah_bot
+        # Kalau ada perintah -> Tembak API X-MNIT
+        time.sleep(1)
